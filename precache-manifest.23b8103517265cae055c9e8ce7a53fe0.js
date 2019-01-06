729d11bb3133fe7e399f1fe0eff8eb3aed51fc6b
wogul <wogul@wp.pl>
2019-01-06 15:49:26 +0100

    Updates

---
+++ precache-manifest.23b8103517265cae055c9e8ce7a53fe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b132853474392b26b06",
+    "url": "/police-stats/static/css/main.db9993f5.chunk.css"
+  },
+  {
+    "revision": "7b132853474392b26b06",
+    "url": "/police-stats/static/js/main.7b132853.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/police-stats/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "57368afb3ba6e7cdca96",
+    "url": "/police-stats/static/js/runtime~main.57368afb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/police-stats/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1253740b870d6e7b012c6749311246a1",
+    "url": "/police-stats/index.html"
+  }
+];
